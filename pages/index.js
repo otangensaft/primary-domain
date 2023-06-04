085eb6f36d46b1ce7c25c9be5b9c82527bb7e864
Danny Tang <tang_daniel@outlook.com>
2023-06-03 18:37:05 -0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          I am the big home page bahahaha
         </h1>
 
         <p className={styles.description}>
